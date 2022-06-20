7602a501ca3d9d1483d568af688a1a8cc7a4ec5f
santosandressa <andressasantosp0@gmail.com>
2022-06-20 17:20:02 -0300

    task 0

---
+++ 0x01/spring_boot_1/src/main/java/com/example/demo/MessageResource.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageResource {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+       return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    @GetMapping("/login")
+    public String login(String user, String password){
+        user = "andressa";
+        password = "12345a";
+
+        if(user.isEmpty() || password.isEmpty()){
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        } else if (user.length() > 15 || password.length() > 15){
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        }
+
+        return  "LOGIN EFETUADO COM SUCESSO !!!";
+    }
+}
