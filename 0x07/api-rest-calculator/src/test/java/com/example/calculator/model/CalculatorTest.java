a3a193aeb13ef241ab41053c202e28aca79ac38a
santosandressa <andressasantosp0@gmail.com>
2022-06-30 15:12:15 -0300

    task 0 - mockmvc

--- 0x07/api-rest-calculator/src/test/java/com/example/calculator/model/CalculatorTest.java
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/model/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.example.calculator.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -8,9 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void sumTest() {
-        Calculator calculator = new Calculator();
+
 
         Double result = calculator.sum(1.0, 2.0);
 
@@ -19,16 +27,12 @@ class CalculatorTest {
 
     @Test
     public void numbersNullSumTest() {
-        Calculator calculator = new Calculator();
-
         assertThrows(NullPointerException.class, () -> calculator.sum(null, 2.0));
     }
 
 
     @Test
     public void subTest() {
-        Calculator calculator = new Calculator();
-
         Double result = calculator.sub(4.0, 1.0);
 
         assertEquals(3.0, result);
@@ -36,8 +40,6 @@ class CalculatorTest {
 
     @Test
     public void divideTest() {
-        Calculator calculator = new Calculator();
-
         Double result = calculator.divide(8.0, 2.0);
 
         assertEquals(4.0, result);
@@ -45,22 +47,17 @@ class CalculatorTest {
 
     @Test
     public void divisionByZeroTest() {
-        Calculator calculator = new Calculator();
-
         assertThrows(ArithmeticException.class, () -> calculator.divide(6.0, 0.0));
     }
 
     @Test
     public void divisionNullTest(){
-        Calculator calculator = new Calculator();
 
         assertThrows(NullPointerException.class, () -> calculator.divide(null, 2.0));
     }
 
     @Test
     public void factorialTest() {
-        Calculator calculator = new Calculator();
-
         Integer result = calculator.factorial(5);
 
         assertEquals(120, result);
@@ -68,8 +65,6 @@ class CalculatorTest {
 
     @Test
     public void integerToBinaryTest() {
-        Calculator calculator = new Calculator();
-
         String result = String.valueOf(calculator.integerToBinary(5));
 
         assertEquals("101", result);
@@ -77,8 +72,6 @@ class CalculatorTest {
 
     @Test
     public void integerToHexadecimalTest() {
-        Calculator calculator = new Calculator();
-
         String result = String.valueOf(calculator.integerToHexadecimal(170));
 
         assertEquals("AA", result);
@@ -86,8 +79,6 @@ class CalculatorTest {
 
     @Test
     public void calculeDayBetweenDateTest() {
-        Calculator calculator = new Calculator();
-
         LocalDate date1 = LocalDate.of(2020, 3, 15);
         LocalDate date2 = LocalDate.of(2020, 3, 29);
 
